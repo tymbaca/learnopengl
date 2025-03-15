2692354247f093e3f7eccf7be4bb254bdb1b4394
Tigran Rostomyan <tumb4ka228@gmail.com>
2025-03-15 14:51:51 +0300

    minor

--- resources/shader/cube.fs
+++ resources/shader/cube.fs
@@ -14,12 +14,15 @@ uniform vec3 lightColor;
 
 vec4 getDeffuse(vec3 fragPos, vec3 normal, vec3 lightPos, vec3 lightColor)
 {
+    normal = normalize(normal);
     vec3 toLight = lightPos - fragPos;
+    float distance = length(toLight);
+    // `-1 to 0` - dark side, `0 to 1` - light side
     float factor = dot(normalize(toLight), normalize(normal));
-    factor = clamp(factor, 0.0, 1.0); // so that dark side will remain ambient (not become more dark)
+
+    factor = max(factor, 0.0); // so that dark side will remain ambient (not become more dark)
     vec3 resultLight = lightColor * factor * 5;
 
-    float distance = length(toLight);
     resultLight /= distance;
 
     return vec4(resultLight, 1.0);
@@ -28,8 +31,8 @@ vec4 getDeffuse(vec3 fragPos, vec3 normal, vec3 lightPos, vec3 lightColor)
 void main()
 {
     vec4 color = mix(texture(ourTexture1, UV), texture(ourTexture2, UV), 0.5);
-    vec4 ambient = color * vec4(ambientLight, 1);
-    vec4 deffuse = color * getDeffuse(FragPos, Normal, lightPos, lightColor);
+    vec4 ambient = vec4(ambientLight, 1);
+    vec4 deffuse = getDeffuse(FragPos, Normal, lightPos, lightColor);
 
-    FragColor = ambient + deffuse;
+    FragColor = (ambient + deffuse) * color;
 }
